9bcfbc55eef6b2ed3264363648a6d6ffc49d9b0d
akihiro <ueyama0105@gmail.com>
2022-06-21 22:39:11 +0900

    HolographicRemoting追加

---
+++ HolographicRemotingOpenXR/Assets/HolographicRemoting/Scripts/AppRemotingSample.cs
@@ -0,0 +1,74 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR;
+
+namespace Microsoft.MixedReality.OpenXR.BasicSample
+{
+    public class AppRemotingSample : MonoBehaviour
+    {
+        [SerializeField] private GameObject connectSettingUI = null;
+        [SerializeField] private InputField textInput = null;
+        [SerializeField] private Text outputText = null;
+        [SerializeField] private Button connectButton = null;
+        [SerializeField] private Button disconnectButton = null;
+        [SerializeField] private Remoting.RemotingConfiguration remotingConfiguration = new Remoting.RemotingConfiguration { RemotePort = 8265, MaxBitrateKbps = 20000 };
+
+        private void Awake()
+        {
+            // Editor実行時はEditor側のRemoting機能を利用
+            if (Application.isEditor)
+            {
+                gameObject.SetActive(false);
+                connectSettingUI.SetActive(false);
+                return;
+            }
+            // 既にHolographicRemoting実行中か確認
+            var XRDisplaySubsystems = new List<XRDisplaySubsystem>();
+            SubsystemManager.GetInstances(XRDisplaySubsystems);
+            foreach (XRDisplaySubsystem xrDisplaySubsystem in XRDisplaySubsystems)
+            {
+                if (xrDisplaySubsystem.running)
+                {
+                    var connectionValid = Remoting.AppRemoting.TryGetConnectionState(out Remoting.ConnectionState connectionState, out Remoting.DisconnectReason disconnectReason);
+                    if (!connectionValid || connectionState == Remoting.ConnectionState.Disconnected)
+                    {
+                        gameObject.SetActive(false);
+                    }
+                    connectSettingUI.SetActive(false);
+                    return;
+                }
+            }
+            // UI設定
+            connectButton.onClick.AddListener(() => StartCoroutine(Remoting.AppRemoting.Connect(remotingConfiguration)));
+            disconnectButton.onClick.AddListener(() => Remoting.AppRemoting.Disconnect());
+            textInput.onValueChanged.AddListener(value => remotingConfiguration.RemoteHostName = value);
+        }
+
+        private void Update()
+        {
+            var ip = $"{remotingConfiguration.RemoteHostName}:{remotingConfiguration.RemotePort}";
+            connectSettingUI.SetActive(true);
+            if (Remoting.AppRemoting.TryGetConnectionState(out Remoting.ConnectionState connectionState, out Remoting.DisconnectReason disconnectReason))
+            {
+                switch (connectionState)
+                {
+                    case Remoting.ConnectionState.Connected:
+                        connectSettingUI.SetActive(false);
+                        outputText.text = $"Connected to {ip}.";
+                        break;
+                    case Remoting.ConnectionState.Connecting:
+                        outputText.text = $"Connecting to {ip}...";
+                        break;
+                    case Remoting.ConnectionState.Disconnected:
+                        outputText.text = $"Disconnected to {ip}. Reason is {disconnectReason}";
+                        break;
+                }
+            }
+            else
+            {
+                outputText.text = $"Disconnected to {ip}. Reason is {disconnectReason}";
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # HolographicRemotingOpenXR
 - Unity 2020.3.35f1
+- MixedRealityToolkit 2.8.0
+
+# MRTK導入
+- MixedRealityFeatureToolから以下をUnityプロジェクトにインポート
+    - Mixed Reality Toolkit Foundation 2.8.0
+    - Mixed Reality Toolkit Standardassets 2.8.0
+    - Mixed Reality OpenXR Plugin 1.4.1
+
+- MRTKの設定からOpenXRを選択
+    ![alt](./document/01.png)
+
+# Unityプロジェクト設定
+- `Build Serrings` の `Platform` は `Windows Standalone` を選択
+- `Player Settings` の `XR Plug-in Management` を以下のように設定
+    ![alt](./document/setting1.png)
+    ![alt](./document/setting2.png)
+
+# Unity Scene設定
+- HoloLens 2アプリと同様に `MixedRealityToolkit`のシーン設定を行う
+- HolographicRemoting用に `HolographicRemoting` プレハブをシーンに追加
+- ![alt](./document/02.png)
